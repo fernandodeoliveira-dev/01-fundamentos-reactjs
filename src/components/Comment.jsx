c97e0bc24095b187f30bb55a6335873a204d4ef1
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2022-09-07 14:40:22 -0300

    Section 3 - Os motores React - Inserindo comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,34 +3,34 @@ import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
-export function Comment() {
-  return (
-    <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
+export function Comment({ content }) {
+    return (
+        <div className={styles.comment}>
+        <Avatar hasBorder={false} src="https://github.com/diego3g.png" alt="" />
 
-      <div className={styles.commentBox}>
-        <div className={styles.commentContent}>
-          <header>
-            <div className={styles.authorAndTime}>
-              <strong>Diego Fernandes</strong>
-              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+        <div className={styles.commentBox}>
+            <div className={styles.commentContent}>
+                <header>
+                    <div className={styles.authorAndTime}>
+                    <strong>Diego Fernandes</strong>
+                    <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+                    </div>
+
+                    <button title="Deletar comentário">
+                    <Trash size={24} />
+                    </button>
+                </header>
+
+                <p>{content}</p>
             </div>
 
-            <button title="Deletar comentário">
-              <Trash size={24} />
+            <footer>
+            <button>
+                <ThumbsUp />
+                Aplaudir <span>20</span>
             </button>
-          </header>
-
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+            </footer>
         </div>
-
-        <footer>
-          <button>
-            <ThumbsUp />
-            Aplaudir <span>20</span>
-          </button>
-        </footer>
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
